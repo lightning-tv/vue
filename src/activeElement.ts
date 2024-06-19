2b767f2a7f0b4676c033d310a097411ce7c20fb2
Chris Lorenzo <chiefcll@gmail.com>
2024-06-18 21:19:24 -0400

    add setActiveElement

--- src/activeElement.ts
+++ src/activeElement.ts
@@ -1,6 +1,8 @@
-import type { ElementNode } from '@lightningtv/core';
+import { Config, type ElementNode } from '@lightningtv/core';
 import { ref } from '@vue/reactivity';
 import type { Ref } from '@vue/reactivity';
 export const activeElement: Ref<ElementNode | undefined> = ref(undefined);
 export const setActiveElement = (val: ElementNode) =>
   (activeElement.value = val);
+
+Config.setActiveElement = setActiveElement;
